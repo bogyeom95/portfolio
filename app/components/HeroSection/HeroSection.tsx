2c4dff74a95751d33e5f6a2b86b58f9b91c4a1a8
bokyum <95bogyeom@gmail.com>
2024-11-21 23:31:18 +0900

    AboutMe 페이지 개발

--- app/components/HeroSection/HeroSection.tsx
+++ app/components/HeroSection/HeroSection.tsx
@@ -1,21 +1,100 @@
-import Image from "next/image";
-import githubLogo from "./assets/github-mark.svg";
+import { ReactNode } from "react";
+import { FaBaby, FaHome, FaUser } from "react-icons/fa";
+import { FaPencil } from "react-icons/fa6";
+import { GiWeightLiftingUp } from "react-icons/gi";
+import { IoIosMail } from "react-icons/io";
+
+type AboutMeItem = {
+  id: number;
+  icon: ReactNode;
+  title: string;
+  description: string;
+};
+
+const aboutMeItems: AboutMeItem[] = [
+  {
+    id: 1,
+    icon: <FaUser className="text-slate-100" />,
+    title: "이름",
+    description: "김보겸",
+  },
+  {
+    id: 2,
+    icon: <FaBaby className="text-slate-100" />,
+    title: "생일",
+    description: "1995년 4월 18일",
+  },
+  {
+    id: 3,
+    icon: <FaHome className="text-slate-100" />,
+    title: "집",
+    description: "경기도 구리시",
+  },
+  {
+    id: 4,
+    icon: <IoIosMail className="text-slate-100" />,
+    title: "메일",
+    description: "95bogyeom@gmail.com",
+  },
+
+  {
+    id: 5,
+    icon: <FaPencil className="text-slate-100" />,
+    title: "학력",
+    description: "강원대학교 (컴퓨터과학과)",
+  },
+  {
+    id: 6,
+    icon: <GiWeightLiftingUp className="text-slate-100" />,
+    title: "취미",
+    description: "웨이트트레이닝",
+  },
+];
 
 export default function HeroSection() {
   return (
-    <section className="w-[100vw] h-[100vh]  relative">
-      <div className="h-full flex flex-col items-center justify-center gap-4">
-        <div className="size-80 relative">
-          <Image
-            className="object-contain"
-            src={githubLogo}
-            fill
-            alt="github log"
-          />
+    <section className="relative w-screen h-screen overflow-hidden">
+      <div className="w-full h-full flex flex-col   gap-4 py-16 px-4 sm:p-20 justify-center">
+        <Horizontal />
+
+        <h1 className="text-2xl sm:text-4xl lg:text-6xl text-slate-100   font-bold">
+          안녕하세요.
+          <br />
+          주니어 개발자 김보겸입니다.
+        </h1>
+
+        <p className="text-sm sm:text-lg lg:text-xl  text-slate-300 mt-4">
+          복잡한 문제를 간단히 해결하는 데 집중하며, <br />
+          현재 프론트엔드 개발에 깊은 관심을 가진 개발자입니다.
+        </p>
+
+        <Horizontal />
+        <div className="w-full bg-gray-800 p-6 rounded-lg shadow-lg">
+          <h1 className="text-2xl sm:text-3xl lg:text-4xl text-white font-bold text-center mb-6 border-b-2 border-slate-500 pb-2">
+            About Me
+          </h1>
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-2 sm:gap-4">
+            {aboutMeItems.map((item) => (
+              <div
+                key={item.id}
+                className="bg-gray-700 p-2 sm:p-4  rounded-md shadow-md flex items-center  gap-4 "
+              >
+                <div className="text-lg sm:text-2xl ">{item.icon}</div>
+                <div className="border-r-2 h-full" />
+                <div>
+                  <h2 className="text-white font-semibold">{item.title}</h2>
+                  <p className="text-slate-300">{item.description}</p>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
-        <h1 className="text-6xl text-white">Bogyeom</h1>
-        <h1 className="text-6xl text-white m-0">FullStack Developer</h1>
+        <Horizontal />
       </div>
     </section>
   );
+}
+
+function Horizontal() {
+  return <hr className="border-t border-2 border-gray-300  w-full" />;
 }
---
+++ app/components/SkillSection/SkillSection.tsx
@@ -0,0 +1,72 @@
+type SkillItem = {
+  name: string; // 기술 이름
+  bg: string; // 배경색 클래스 (TailwindCSS)
+  text: string; // 텍스트 색상 클래스 (TailwindCSS)
+};
+
+type Skills = {
+  [category: string]: SkillItem[]; // 각 카테고리는 SkillItem 배열
+};
+
+const skills: Skills = {
+  language: [
+    { name: "Typescript", bg: "bg-blue-700", text: "text-white" },
+    { name: "JavaScript", bg: "bg-yellow-500", text: "text-black" },
+    { name: "Java", bg: "bg-red-600", text: "text-white" },
+  ],
+  frontend: [
+    { name: "React", bg: "bg-blue-400", text: "text-white" },
+    { name: "Next.js", bg: "bg-gray-900", text: "text-white" },
+    { name: "Bootstrap", bg: "bg-blue-500", text: "text-white" },
+    { name: "TailwindCSS", bg: "bg-teal-500", text: "text-white" },
+  ],
+  backend: [
+    { name: "Spring", bg: "bg-green-700", text: "text-white" },
+    { name: "JPA", bg: "bg-indigo-600", text: "text-white" },
+    { name: "QueryDSL", bg: "bg-teal-600", text: "text-white" },
+    { name: "MySQL", bg: "bg-blue-500", text: "text-white" },
+    {
+      name: "MongoDB ",
+      bg: "bg-green-600",
+      text: "text-white",
+    },
+  ],
+  etc: [
+    { name: "NCP", bg: "bg-gray-800", text: "text-white" },
+    { name: "AWS ", bg: "bg-orange-600", text: "text-white" },
+    { name: "GitHub Actions", bg: "bg-purple-600", text: "text-white" },
+    { name: "Docker", bg: "bg-blue-600", text: "text-white" },
+    {
+      name: "Watchtower (Docker Update Manager)",
+      bg: "bg-gray-600",
+      text: "text-white",
+    },
+  ],
+};
+
+export default function SkillSection() {
+  return (
+    <div className="mt-8 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+      {Object.entries(skills).map(([category, items]) => (
+        <div
+          key={category}
+          className="p-4 bg-gray-800 rounded-lg shadow-lg flex flex-col"
+        >
+          <h2 className="text-lg sm:text-xl lg:text-2xl font-semibold text-slate-100 capitalize mb-2">
+            {category}
+          </h2>
+          <div className="flex flex-wrap gap-2">
+            {items.map((item, index) => (
+              <span
+                key={index}
+                className={`px-4 py-2 rounded-lg ${item.bg} ${item.text} text-sm sm:text-base`}
+              >
+                {item.name}
+              </span>
+            ))}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
