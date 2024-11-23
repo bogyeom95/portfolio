bc8e88260632482143585212833ef7bea07dae01
bokyum <95bogyeom@gmail.com>
2024-11-23 19:21:13 +0900

    HeroSection ScrollTrigger 적용

--- app/components/HeroSection/HeroSection.tsx
+++ app/components/HeroSection/HeroSection.tsx
@@ -3,35 +3,48 @@ import React from "react";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { animateContainer, animateSplitText } from "../animations";
-import { animateAsciiBackgrounds } from "./heroAnimation";
 import AsciiBackground from "./components/AsciiBackground";
 import { ascii } from "./ascii";
+import SplitType from "split-type";
 gsap.registerPlugin(ScrollTrigger);
 
 export default function HeroSection() {
   const containerRef = React.useRef<HTMLDivElement | null>(null);
   const titleRef = React.useRef<HTMLDivElement | null>(null);
-
   const bgRefs = React.useRef<(HTMLDivElement | null)[]>([]);
 
   useGSAP(() => {
-    const tl = gsap.timeline();
-
-    tl.add(animateContainer(containerRef))
-      .add(animateSplitText(titleRef, { y: -30 }), 0)
-      .add(animateAsciiBackgrounds(bgRefs.current), 0);
+    if (titleRef.current) {
+      const titleAnimation = gsap.timeline();
+      const titleSplitText = new SplitType(titleRef.current, {
+        types: "chars",
+      });
+      titleAnimation.from(titleSplitText.chars, {
+        y: gsap.utils.wrap([-30, 30]),
+        opacity: 0,
+        filter: "blur(3px)",
+        scale: 3.0,
+        stagger: {
+          each: 0.1,
+          from: "center",
+        },
+      });
+    }
 
     ScrollTrigger.create({
       trigger: containerRef.current,
-      start: "top center",
-      end: "bottom center",
-
-      markers: true,
-      animation: tl,
-
-      // defaults enter leave enterBack leaveBack
-      toggleActions: "restart pause restart pause",
+      start: "top top",
+      end: "+=2000",
+      animation: gsap.from(bgRefs.current, {
+        scale: 0,
+        opacity: 0,
+        stagger: {
+          each: 0.1,
+          from: "end",
+        },
+      }),
+      pin: true,
+      scrub: true,
     });
   });
 
@@ -40,14 +53,17 @@ export default function HeroSection() {
       ref={containerRef}
       className="relative w-screen h-screen flex flex-col justify-between"
     >
-      {/* 콘텐츠 영역 */}
-      <div className="relative z-10 flex items-center justify-center w-full h-full flex-col gap-4 text-white ">
-        <h1
+      <div className="relative z-10 flex items-center justify-center w-full h-screen flex-col gap-4 text-white ">
+        <span
           ref={titleRef}
-          className=" text-4xl sm:text-6xl lg:text-8xl font-bold"
+          className="text-5xl sm:text-7xl lg:text-9xl font-extrabold leading-tight tracking-tight text-white drop-shadow-lg"
         >
-          My Portfolio
-        </h1>
+          BOGYEOM
+          <br />
+          <span className="text-5xl sm:text-7xl lg:text-9xl font-extrabold leading-tight tracking-tight text-white drop-shadow-lg">
+            PORTFOLIO
+          </span>
+        </span>
       </div>
 
       <AsciiBackground
