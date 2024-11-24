f274dfd4d5213d651ff1d634c55415271e068980
bokyum <95bogyeom@gmail.com>
2024-11-24 17:35:24 +0900

    Ascii text size 조절

--- app/components/AsciiBackground.tsx
+++ app/components/AsciiBackground.tsx
@@ -58,7 +58,7 @@ function AsciiBackground({
 }) {
   return (
     <div
-      className={`absolute max-w-[100vw] max-h-[100vh]  bottom-0 ${xPosition} text-gray-500 whitespace-pre text-[4px] md:text-[6px]  overflow-hidden`}
+      className={`absolute max-w-[100vw] max-h-[100vh]  bottom-0 ${xPosition} text-gray-500 whitespace-pre text-[3px] md:text-[5px]  overflow-hidden`}
       style={{
         display: "grid",
         gridTemplateColumns: `repeat(${width / blockSize}, auto)`, // 블록 열 개수
